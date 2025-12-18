01ad9c48d782c5566c02551375e784f7c2a1c3dd
Aditya Bhattacharjee <a.bhattacharjee@qmul.ac.uk>
2025-12-18 20:08:56 +0000

    minor fix

--- tools/generate_embeddings.py
+++ tools/generate_embeddings.py
@@ -188,18 +188,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--audio_dirs", nargs="+", required=True)
-    parser.add_argument("--tag_metadata", type=str, default="../tag_metadata.json")
-    parser.add_argument("--exclude_tag_list", type=str, default="../exclude_tag_list.txt")
+    parser.add_argument("--tag_metadata", type=str, default="tag_metadata.json")
+    parser.add_argument("--exclude_tag_list", type=str, default="exclude_tag_list.txt")
 
     parser.add_argument("--checkpoint", type=str)
-    parser.add_argument("--onnx_model", type=str, default="../models/qvim.onnx")
+    parser.add_argument("--onnx_model", type=str, default="models/qvim.onnx")
 
     parser.add_argument("--pretrained_name", type=str, default="mn10_as")
     parser.add_argument("--batch_size", type=int, default=128)
 
     parser.add_argument("--output_npy", type=str, default="/data/scratch/acw723/laion_fsd_excluded_embeddings.npy")
     parser.add_argument("--output_metadata", type=str, default="laion_fsd_excluded_metadata.csv")
-    parser.add_argument("--freesound_meta_csv", type=str, default="../freesound_meta.csv")
+    parser.add_argument("--freesound_meta_csv", type=str, default="freesound_meta.csv")
 
     args = parser.parse_args()
 
